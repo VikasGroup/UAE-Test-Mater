67fa3c3f3a4a601d45e1ad0014874350f2f67c39
Pavithri <Pavithri@Pavithri-PC>
2017-02-09 10:35:46 +0530

    1st

--- src/test/java/net/uae/test/uae/test/steps/LoginSteps.java
+++ src/test/java/net/uae/test/uae/test/steps/LoginSteps.java
@@ -1,14 +1,7 @@
 package net.uae.test.uae.test.steps;
 
 import org.junit.Assert;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-
-
-
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
